0d8e8d15682fafa992b4c27c8d04baaa407168c2
Eric Laursen <ericjlaursen@gmail.com>
2019-10-09 06:39:35 -0400

    prod && dev separate bundles

---
+++ build-utils/webpack.production.js
@@ -0,0 +1,7 @@
+module.exports = () => ({
+  output: {
+  	// makes the filename scrambled
+  	//... incld the src string in the index.html 
+    filename: "[chunkhash].js"
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const wpMerge = require("webpack-merge");
+
+/*
+  modeConfig
+  returns a fn that takes an arg
+  returns a require
+  that returns a fn that is called
+
+  looks for webpack.production OR webpack.development
+  TAKEN from env.mode from the package.json
+*/
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+
+/*
+  Includes defaults (mode && presets)
+*/
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
+  /*
+    Webpack Merge
+    https://github.com/survivejs/webpack-merge
+    like object.assign, accept works with the webpack config files
+
+    can take a...
+    - default
+    - any EXTRAS
+
+    NOW the prod build config can be separate from dev build config
+
+    Use NPM RUN PROD or NPM RUN DEV
+  */
+  return wpMerge(
+    {
+      mode,                   //mode from 'props'
+      output: {
+        filename: "bundle.js"
+      },
+      plugins: [
+        new HtmlWebpackPlugin(), 
+        //shows webpack build process during build
+        // as terminal output
+        new webpack.ProgressPlugin()
+      ]
+    },
+
+    //returns production OR development configuration
+    modeConfig(mode)
+  );
+};
