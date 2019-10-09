c10cf07be1ad104e81ed78c36216b69633e355ca
Eric Laursen <ericjlaursen@gmail.com>
2019-10-09 06:57:29 -0400

    updates css loading

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,7 +1,28 @@
+//https://webpack.js.org/plugins/mini-css-extract-plugin/
+const MiniCssExtract = require('mini-css-extract-plugin')
+
 module.exports = () => ({
   output: {
   	// makes the filename scrambled
   	//... incld the src string in the index.html 
-    filename: "[chunkhash].js"
-  }
+  	// .. 93298cf39b8093536b08.js
+    // filename: "[chunkhash].js"
+    filename: "bundle.js"
+  },
+  module: {
+  	rules: [
+  		{
+  			test: /\.css$/,
+  			/*
+  				In Dev this is
+  				use: ["style-loader", "css-loader"]
+  				THIS puts the styles in a css file in the dist folder
+  			*/
+  			use: [MiniCssExtract.loader, "css-loader"]
+  		}
+  	]
+  },
+  plugins: [
+  	new MiniCssExtract()
+  ]
 });
